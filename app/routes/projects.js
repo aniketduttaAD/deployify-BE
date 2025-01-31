58f81970a61b0624793652295cf69c3e5432fd6d
Aniket Dutta <aniketdutta@Anikets-MacBook-Air.local>
2025-01-31 23:03:33 +0530

    minor fixes

--- app/routes/projects.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { uploadProject } = require('../controllers/projects');
-const router = express.Router();
-
-router.post('/upload', uploadProject);
-
-module.exports = router;
